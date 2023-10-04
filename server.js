6658cbec23443097f44570b8ce660aa8286f94e1
Nekhlesh SIngh <38405081+Nekhlesh1@users.noreply.github.com>
2023-10-05 00:18:57 +0530

    server creation

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const port = process.env.port ||8000;
+
+
+// CREATING SERVER
+http.listen(port, (err) => {
+    if(err)
+    {
+        return console.log("Error connecting the server", port);
+    }
+    return console.log("Successsfully connected to the server at port :",port );
+})
+
+// SETTING ROUTES
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+ '/index.html')
+})
